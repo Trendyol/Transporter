6bb35b3453325b15847e50aa5a30c423df738721
firat.komurcu <firat.komurcu@trendyol.com>
2021-07-12 21:16:01 +0300

    remove unnecessary updateSetting function from TestStartup.cs

--- Transporter.IntegrationTests/Base/TestStartup.cs
+++ Transporter.IntegrationTests/Base/TestStartup.cs
@@ -22,21 +22,11 @@ namespace Transporter.IntegrationTests.Base
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
 
-            UpdateAppSetting("test1", "55");
             services.AddTransient<ICouchbaseProviderHelper, CouchbaseProviderHelper>();
             services.AddTransient<ICouchbaseProvider, CouchbaseProvider>();
             services.AddTransient<IBucketProvider, BucketProvider>();
 
             services.AddSingleton(configuration);
-        }
-        
-        public static void UpdateAppSetting(string key, string value)
-        {
-            var configJson = File.ReadAllText("appsettings.json");
-            var config = JsonSerializer.Deserialize<Dictionary<string, object>>(configJson);
-            config[key] = value;
-            var updatedConfigJson = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText("appsettings.json", updatedConfigJson);
         }
 
         public static void Configure(IApplicationBuilder app)
