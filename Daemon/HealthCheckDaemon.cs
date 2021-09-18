b153a37984af2ee1d4a8e75f6b4a100bc6b83d28
firat.komurcu <firat.komurcu@trendyol.com>
2021-09-18 22:52:42 +0300

    add HealthCheckDaemon.cs

---
+++ Daemon/HealthCheckDaemon.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TransporterService.Daemon
+{
+    [ExcludeFromCodeCoverage]
+    public class HealthCheckDaemon : BackgroundService
+    {
+        private const int ListeningPort = 80;
+        private static readonly byte[] Message = Encoding.ASCII.GetBytes("Pong");
+        private readonly ILogger<HealthCheckDaemon> _logger;
+
+        public HealthCheckDaemon(ILogger<HealthCheckDaemon> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                server = new TcpListener(IPAddress.Any, ListeningPort);
+
+                server.Start();
+
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    var receiving = await Receive(server);
+
+                    await Respond(stoppingToken, receiving.Stream);
+                    
+                    receiving.Client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogDebug($"Health check listener error message: {e.Message}");
+            }
+            finally
+            {
+                server?.Stop();
+            }
+        }
+
+        private async Task<(TcpClient Client, Stream Stream)> Receive(TcpListener server)
+        {
+            var client = await server.AcceptTcpClientAsync();
+
+            var stream = client.GetStream();
+
+            return (client, stream);
+        }
+
+        private async Task Respond(CancellationToken stoppingToken, Stream stream)
+        {
+            var buffer = new byte[256];
+            
+            while ((await stream.ReadAsync(buffer, 0, buffer.Length, stoppingToken)) != 0)
+            {
+                await stream.WriteAsync(Message, 0, Message.Length, stoppingToken);
+            }
+        }
+    }
+}
