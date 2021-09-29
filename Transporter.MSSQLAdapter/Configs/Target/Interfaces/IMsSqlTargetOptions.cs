6edd19893f83c178448bbe096fadcee96e4f54c2
firat.komurcu <firat.komurcu@trendyol.com>
2021-09-29 23:02:47 +0300

    add if not exists condition to mssql insert. resolves #15

--- Transporter.MSSQLAdapter/Configs/Target/Interfaces/IMsSqlTargetOptions.cs
+++ Transporter.MSSQLAdapter/Configs/Target/Interfaces/IMsSqlTargetOptions.cs
@@ -6,5 +6,6 @@ namespace Transporter.MSSQLAdapter.Configs.Target.Interfaces
         public string Schema { get; set; }
         public string ConnectionString { get; set; }
         public string ExcludedColumns { get; set; }
+        public string IdColumn { get; set; }
     }
 }
