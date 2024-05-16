51e56cda8119f6fb2103c051e1656404a67cca52
fikretasma <fikret.asma@gmail.com>
2024-05-16 09:02:02 +0300

    #12 review fixes for JobConstants

--- Transporter.IntegrationTests/Constants/JobConstants.cs
+++ Transporter.IntegrationTests/Constants/JobConstants.cs
@@ -13,14 +13,14 @@ public static class JobConstants
     
     public static class CouchbaseToMsSqlJob
     {
-        public const string SourceBucketName = "test-source-bucket-couchbase-to-msql";
+        public const string SourceBucketName = "test-source-bucket-couchbase-to-mssql";
         public const string AppSettingsFileName = "appsettings_couchbaseToMsSql.json";
     }
     public static class MsSqlToCouchbase
     {
-        public const string InterimBucketName = "test-interim-bucket-couchbase-to-mssql";
-        public const string InterimBucketIndexName = "test_interim_bucket_couchbase_to_mssql_index";
-        public const string TargetBucketName = "test-target-bucket-couchbase-to-mssql";
+        public const string InterimBucketName = "test-interim-bucket-mssql-to-couchbase";
+        public const string InterimBucketIndexName = "test_interim_bucket_mssql_to_couchbase_index";
+        public const string TargetBucketName = "test-target-bucket-mssql-to-couchbase";
         public const string AppSettingsFileName = "appsettings_MsSqlToCouchbase.json";
     }
 
